35c60330358013e2990bc1f94090b79e38b72e36
ItsQuasi <truequasi2010@gmail.com>
2025-07-12 12:38:14 +0700

    Day 4

--- assets/main.js
+++ assets/main.js
@@ -4,25 +4,24 @@ const questions = [
     { q: "They ___ in the classroom.", o: ["is", "are", "am", "be"], a: "are" },
     { q: "We ___ ready.", o: ["is", "are", "am", "be"], a: "are" },
     { q: "He ___ from Vietnam.", o: ["am", "is", "are", "were"], a: "is" },
-
     { q: "You ___ very nice.", o: ["is", "am", "are", "be"], a: "are" },
     { q: "It ___ a cat.", o: ["are", "is", "am", "be"], a: "is" },
     { q: "I ___ not hungry.", o: ["is", "am", "are", "be"], a: "am" },
     { q: "They ___ not here.", o: ["is", "are", "am", "be"], a: "are" },
     { q: "We ___ not tired.", o: ["am", "is", "are", "be"], a: "are" },
-
+    
     { q: "___ she your teacher?", o: ["Is", "Are", "Am", "Do"], a: "Is" },
     { q: "___ they your friends?", o: ["Is", "Are", "Am", "Do"], a: "Are" },
     { q: "___ I late?", o: ["Am", "Is", "Are", "Be"], a: "Am" },
     { q: "___ it your dog?", o: ["Is", "Are", "Am", "Was"], a: "Is" },
-    { q: "He ___ not at school.", o: ["am", "is", "are", "be"], a: "is" },
-
-    { q: "We ___ students.", o: ["is", "are", "am", "be"], a: "are" },
-    { q: "You ___ not my brother.", o: ["is", "am", "are", "be"], a: "are" },
-    { q: "I ___ in class now.", o: ["is", "are", "am", "be"], a: "am" },
-    { q: "She ___ not happy today.", o: ["is", "am", "are", "was"], a: "is" },
-    { q: "___ you okay?", o: ["Are", "Is", "Am", "Do"], a: "Are" }
+    { q: "___ you okay?", o: ["Are", "Is", "Am", "Do"], a: "Are" },
+    { q: "___ he at school?", o: ["Is", "Are", "Am", "Do"], a: "Is" },
+    { q: "___ we students?", o: ["Are", "Is", "Am", "Be"], a: "Are" },
+    { q: "___ you my brother?", o: ["Are", "Is", "Am", "Be"], a: "Are" },
+    { q: "___ I in class now?", o: ["Am", "Is", "Are", "Be"], a: "Am" },
+    { q: "___ she happy today?", o: ["Is", "Am", "Are", "Was"], a: "Is" }
 ];
+
 
 let shuffledQuestions;
 let current = 0;
